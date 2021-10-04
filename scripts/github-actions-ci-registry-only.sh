4dbd1a9022ec5c7d8147d64c8264fe92517d1a58
Josh Dolitsky <josh@dolit.ski>
2021-10-04 16:28:35 -0500

    test for PR 10211
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- scripts/github-actions-ci-registry-only.sh
+++ scripts/github-actions-ci-registry-only.sh
@@ -29,7 +29,7 @@ export GITHUB_SHA="${GITHUB_SHA:-latest}"
 which helm || true
 mkdir -p /tmp/gopath/src/helm.sh
 pushd /tmp/gopath/src/helm.sh
-git clone https://github.com/bloodorangeio/helm.git -b hip-6
+git clone https://github.com/Ka0o0/helm.git -b fix-registry-config-path
 pushd helm/
 GOPATH=/tmp/gopath make build
 popd
